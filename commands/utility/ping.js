637d83808fc7e7eb0f96553306d7919fe6b057a0
Consulus <condruposeidon@gmail.com>
2024-08-05 23:45:07 -0400

    Upgrade ping command to differentiate websocket & api ping

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -4,6 +4,6 @@ module.exports = {
     .setName('ping')
     .setDescription('Pings the bot'),
 async execute(interaction) {
-    await interaction.reply("Ping...").then((msg)=> {msg.edit("Pong! `"+(Date.now()-msg.createdTimestamp)+"ms`")});
+    await interaction.reply("Ping...").then((msg)=> {msg.edit("Pong! \nAPI ping: `"+(Date.now()-msg.createdTimestamp)+"ms` \nWebsocket Ping: `"+interaction.client.ws.ping+"ms`" )});
 }
 }
