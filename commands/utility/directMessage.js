520784ea65c06dbefea52811712ba69c2be794ac
Consulus <condruposeidon@gmail.com>
2024-08-06 00:35:08 -0400

    Add embed for DM received

--- commands/utility/directMessage.js
+++ commands/utility/directMessage.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('dm')
@@ -11,20 +11,46 @@ module.exports = {
         option.setName('target')
             .setDescription('User that will be messaged!')
             .setRequired(true)),
-    async execute(interaction) {
+    async execute(interaction, client) {
         const user = interaction.options.getUser('target');
         const message = interaction.options.getString('message');
+        let dmSuccess = false
+
 
     if (interaction.user.id == '412367471245393923'){
         try {
         await user.send(message);
-        await interaction.reply({content: 'DM sent successfully!', ephemeral: true})
+        await interaction.reply({content: 'DM sent successfully!', ephemeral: true});
+        dmSuccess = true;
     } catch (error) {
         console.error('Error sending DM ', error);
         await interaction.reply({content: 'An error occurred sending the DM!', ephemeral: true})
     }} else{
         await interaction.reply({content: 'You do not have permission to execute this command!', ephemeral: true})
         console.log('Someone attempted to execute dm command but did not have Owner: ', interaction.user.tag )
+    }
+
+    if (dmSuccess) {
+        const embed = new EmbedBuilder()
+        .setColor('#03fcf8')
+        .setAuthor({name: 'DM Sent!', iconURL: user.displayAvatarURL()})
+        .addFields(
+            {name: 'Sent to', value: `Name: ${user.displayName} | Tag: ${user.tag} | ID: (${user.id}) |` },
+            {name: 'Content', value: message}
+        )
+        .setFooter({text: 'Botsulus DM detection, contact Consu1us for assistance'})
+        .setTimestamp();
+    try {
+        const logChannel = client.channels.cache.get('1270019192158031902');
+        if (logChannel) {
+            await logChannel.send({embeds: [embed]});
+            console.log('DM logged successfully! Sent to:', user.displayName,'| Message:', message);
+        } else {
+            console.error('Log channel not found!');
+        }
+    } catch (error) {
+        console.error('Error with DM detection', error);
+    }
     }
 }
 }
